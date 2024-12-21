6d87bc618a7f37f59328a27fad71ce6997dbfc5f
Cainã Pimenta <cainapime@gmail.com>
2024-12-21 06:26:01 -0300

    Associação estudante/matrícula

--- src/database/models/pessoa.js
+++ src/database/models/pessoa.js
@@ -14,6 +14,7 @@ module.exports = (sequelize, DataTypes) => {
 
 			Pessoa.hasMany(models.Matricula, {
 				foreignKey: 'estudante_id',
+				scope: { status: 'matriculado' },
 				as: 'aulasMatriculadas'
 			});
 		}
